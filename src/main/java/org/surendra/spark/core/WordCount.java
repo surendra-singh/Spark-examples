c4d009d6afc7400bd9d9664e81caed8c8398c303
surendra.singh <surendra.nmc@gmail.com>
2015-04-21 11:31:37 +0530

    changed package decleration

--- src/main/java/org/surendra/spark/core/WordCount.java
+++ src/main/java/org/surendra/spark/core/WordCount.java
@@ -1,10 +1,11 @@
 /**
  * 
  */
-package org.surendra.spark;
+package org.surendra.spark.core;
 
 import java.util.Arrays;
 
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -21,10 +22,14 @@ public class WordCount {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		final JavaSparkContext sc = new JavaSparkContext("local", "WordCount");
-		final JavaRDD<String> lines = sc.textFile("/home/surendra/Sample/Spark.txt");
+		SparkConf sparkConf = new SparkConf();
+		sparkConf.setMaster("local[*]");
+		sparkConf.setAppName("Word Count");
+		
+		JavaSparkContext sc = new JavaSparkContext(sparkConf);
+		JavaRDD<String> lines = sc.textFile("/home/surendra/Desktop/sample.txt");
 
-		final JavaPairRDD<String, Integer> tuple = lines.flatMap(l -> Arrays.asList(l.split(" ")))
+		JavaPairRDD<String, Integer> tuple = lines.flatMap(l -> Arrays.asList(l.split(" ")))
 				.mapToPair(w -> new Tuple2<String, Integer>(w, 1)).reduceByKey((a, b) -> a + b).sortByKey(); 
 		
 		for (Tuple2<String, Integer> tuple2 : tuple.toArray()) {
