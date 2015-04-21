aabdfb869035638e7ee3ee1558d5b5f4eb3c5437
surendra.singh <surendra.nmc@gmail.com>
2015-04-21 12:08:57 +0530

    Mongo Data Read

--- src/main/java/org/surendra/spark/core/WordCount.java
+++ src/main/java/org/surendra/spark/core/WordCount.java
@@ -22,17 +22,34 @@ public class WordCount {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		/**
+		 * Create SparkConfig Object and set master URL and Application Name
+		 */
 		SparkConf sparkConf = new SparkConf();
 		sparkConf.setMaster("local[*]");
 		sparkConf.setAppName("Word Count");
 		
+		/**
+		 * Create JavaSparkContex Object, passing SparkConfig as argument  
+		 */
 		JavaSparkContext sc = new JavaSparkContext(sparkConf);
+		
+		/**
+		 * Load sample data as text file. Spark will return RDD of String.
+		 * Each line in file represent one record in RDD
+		 */
 		JavaRDD<String> lines = sc.textFile("/home/surendra/Desktop/sample.txt");
 
+		/**
+		 * Word count after, map and reduce occurrence of each word
+		 */
 		JavaPairRDD<String, Integer> tuple = lines.flatMap(l -> Arrays.asList(l.split(" ")))
 				.mapToPair(w -> new Tuple2<String, Integer>(w, 1)).reduceByKey((a, b) -> a + b).sortByKey(); 
 		
-		for (Tuple2<String, Integer> tuple2 : tuple.toArray()) {
+		/**
+		 * Collect the Tuples of word and their count and print
+		 */
+		for (Tuple2<String, Integer> tuple2 : tuple.collect()) {
 			System.out.println(tuple2._1() + " - " + tuple2._2());
 		}
 		sc.close();
---
+++ src/main/java/org/surendra/spark/mongo/MongoRead.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package org.surendra.spark.mongo;
+
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.bson.BSONObject;
+import org.surendra.spark.entity.User;
+
+import com.mongodb.hadoop.MongoInputFormat;
+
+/**
+ * Reading data from mongoDb using mongo-hadoop third party library
+ * 
+ * @author surendra.singh
+ *
+ */
+public class MongoRead {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * Hadoop Configuration to define mongoDB properties
+		 */
+		Configuration mongodbConfig = new Configuration();
+		
+		/**
+		 * Set the Input format to load the data
+		 */
+		mongodbConfig.set("mongo.job.input.format", "com.mongodb.hadoop.MongoInputFormat");
+		
+		/**
+		 * Set mongoDB URI for - mongodb://<host-name>:<port>/<database>.<collection-name>
+		 */
+		mongodbConfig.set("mongo.input.uri", "mongodb://localhost:27017/test.user");
+
+		SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("Mango Data Read");
+		JavaSparkContext context = new JavaSparkContext(conf);
+
+		/**
+		 * Read the data from mongoDb using SparkContext object, it returns JavaPairRDD
+		 * Object - mongoDB ObjectId
+		 * BSONOBJECT - mongoDB document
+		 */
+		JavaPairRDD<Object, BSONObject> documents = context.newAPIHadoopRDD(
+				mongodbConfig, MongoInputFormat.class, Object.class, BSONObject.class);
+
+		/**
+		 * Map loaded BSONObject to custom objects
+		 */
+		@SuppressWarnings("resource")
+		JavaRDD<User> userRDD = documents.map(t -> {
+			User user = new User();
+			user.setName(t._2().get("name").toString());
+			user.setLocation(t._2().get("location").toString());
+			user.setAge((int) t._2().get("age"));
+			return user;
+		});
+
+		/**
+		 * Print user information fetched from mongoDb
+		 */
+		List<User> userList = userRDD.collect();
+		for (User user : userList) {
+			System.out.println("**************************************************");
+			System.out.println("User Name - " + user.getName());
+			System.out.println("User Age - " + user.getAge());
+			System.out.println("User Location - " + user.getLocation());
+		}
+		
+		context.close();
+	}
+}
